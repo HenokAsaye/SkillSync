dc550685e3435cd76601bdaf0695295d9a78f017
Aklile Ansa <aklilemente72@gmail.com>
2025-08-09 20:04:39 +0300

    Initial commit

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+
+
+export default function Home() {
+  return (
+    <button className="btn btn-primary">click me mf</button>
+  );
+}
---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,15 @@
+import daisyui from "daisyui"
+
+const config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+}
+
+export default config
